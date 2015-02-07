f9c53ef9e4ca91a346ebd19411ff0977cc50c6c8
Rob Simpson <robertsimpson201291@gmail.com>
2015-02-07 21:49:26 +0000

    added pricing page images and planner section

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,13 +4,15 @@ require.config({
 	baseUrl: '.',
 	waitSeconds: 0,
 	paths: {
-		FastClick: '../_components/fastclick/lib/fastclick'
+		FastClick: '../_components/fastclick/lib/fastclick',
+		ScrollMonitor: '../_components/scrollMonitor/scrollMonitor'
 	},
 	modules: [
 		{
 			name: 'main',
 			include: [
-				'FastClick'
+				'FastClick',
+				'ScrollMonitor'
 			]
 		}
 	]
